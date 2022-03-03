ca928abe86841ec9534a54aaee654b7968097959
Muhammad Hassan Shaikh <85787122+MuhammadHassanShaikh@users.noreply.github.com>
2022-03-03 06:49:29 -0800

    Update variable.java

--- variable.java
+++ variable.java
@@ -24,3 +24,50 @@ class A
         System.out.println(c);
     }
 }
+
+//****************CONTAINER WIDGET PART 2
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hassan'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text(
+              'hello',
+              style: TextStyle(fontSize: 30),
+            ),
+            // color: Colors.orange,
+            height: 200,
+            width: 200,
+            alignment: Alignment.center,
+            //padding: EdgeInsets.only(top: 20, bottom: 20),
+            // padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            // margin: EdgeInsets.all(30),
+            // constraints: BoxConstraints(maxHeight: 100, maxWidth: 150),
+            // transform: Matrix4.rotationZ(25.0),
+            // transform: Matrix4.skewX(3.0)..rotateZ(20.0),
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              // border: Border.all(
+              //   color: Colors.black,
+              //   width: 3,
+              //   style: BorderStyle.solid,
+              // )
+              border: Border(
+                top: BorderSide(color: Colors.black, width: 2),
+                bottom: BorderSide(color: Colors.black, width: 4),
+                left: BorderSide(color: Colors.black, width: 4),
+                right: BorderSide(color: Colors.black, width: 4),
+              )
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
