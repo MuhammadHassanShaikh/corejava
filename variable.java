de8eec0245cde1cbbdce1e2c074fbfefbdf8586e
Muhammad Hassan Shaikh <85787122+MuhammadHassanShaikh@users.noreply.github.com>
2022-03-03 08:24:12 -0800

    Update variable.java

--- variable.java
+++ variable.java
@@ -2769,6 +2769,8 @@ void main() {
     ),
   );
 }
+    
+    
 import 'package:flutter/material.dart';
 
 void main()
@@ -2882,3 +2884,68 @@ void main() {
 //     ),
 //   );
 // }
+
+    
+    
+ //****************CONTAINER WIDGET PART 2
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hassan'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text(
+              'hello',
+              style: TextStyle(fontSize: 30),
+            ),
+            // color: Colors.orange,
+            height: 200,
+            width: 200,
+            alignment: Alignment.center,
+            //padding: EdgeInsets.only(top: 20, bottom: 20),
+            // padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            // margin: EdgeInsets.all(30),
+            // constraints: BoxConstraints(maxHeight: 100, maxWidth: 150),
+            // transform: Matrix4.rotationZ(25.0),
+            // transform: Matrix4.skewX(3.0)..rotateZ(20.0),
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              border: Border.all(
+                color: Colors.black,
+                width: 3,
+                style: BorderStyle.solid,
+              ),
+              // borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                //  borderRadius: BorderRadius.only(
+                //    topLeft: Radius.circular(25),
+                //    bottomLeft: Radius.circular(25),
+                //  ),
+              // border: Border(
+              //   top: BorderSide(color: Colors.black, width: 2),
+              //   bottom: BorderSide(color: Colors.black, width: 4),
+              //   left: BorderSide(color: Colors.black, width: 4),
+              //   right: BorderSide(color: Colors.black, width: 4),
+              // )
+                  // boxShadow: [
+                  //   BoxShadow(blurRadius: 50.0, color: Colors.grey,
+                  //   spreadRadius: 20.0,
+                  //   offset: Offset(5.0, 5.0)
+                  //   )
+                  // ]
+                  // shape: BoxShape.circle,
+                  gradient: LinearGradient
+                  (begin: Alignment.centerRight, end: Alignment.centerLeft,
+                  colors: [Colors.yellow, Colors.green, Colors.pink] 
+                  )
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
